6fc0e388793e921a42b0c92e7558476fc23a5ddb
EDRIC BOBY <boby.triraharjo@gmail.com>
2023-11-14 00:12:01 +0700

    First Initial Commit

---
+++ scripts.js
@@ -0,0 +1,40 @@
+window.onload = function() {
+    loadData()
+    alert("Page loaded!");
+};
+
+
+
+function loadData() {
+    var table = document.getElementById("display-table");
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.onload = function() {
+        if (xhr.status == 200) {
+            const data = JSON.parse(xhr.responseText);
+            
+            const colRow = document.createElement("tr");
+            for (let col_names of Object.keys(data[0])) {
+                const colCell = document.createElement("th");
+                colCell.textContent = col_names;
+                colRow.appendChild(colCell);
+            };
+            table.appendChild(colRow);
+
+            for (let item of data) {
+                const dataRow = document.createElement("tr");
+                for (let content of Object.values(item)) {
+                    const dataCell = document.createElement("td");
+                    dataCell.textContent = content;
+                    dataRow.appendChild(dataCell);
+                }
+                table.appendChild(dataRow);
+            };
+        };
+    };
+
+    xhr.open('GET', 'https://edric-raharjo.alwaysdata.net/UTS/data/', true);
+    xhr.send();
+
+};
---
+++ scripts.php
@@ -0,0 +1,15 @@
+<?php
+    $f_name = $_POST['F_Name']; 
+    $l_name = $_POST['L_Name']; 
+    $occ = $_POST['Occupation']; 
+
+    $information = "\n" . "300" . ',' .  $f_name . ',' . $l_name . ',' . ".@unair.ac.id". ',' . "undefined.undefined@ftmm.unair.ac.id" . ',' . $occ;
+
+    if (isset($_POST["submit"])) {
+        file_put_contents(
+            "test.csv",
+            $information,
+            FILE_APPEND 
+        );
+    };
+?>
